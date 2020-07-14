59595ee9366529c9bc2a6de0ccabd3cd84b1d12a
Logan Metzger <loganmetzger@Logan-Metzger.local>
2020-07-14 15:55:27 -0400

    Finished work on posts file, but there's a lot of errors. I think they stem from the individual post file

--- src/components/Posts/Post.js
+++ src/components/Posts/Post.js
@@ -24,7 +24,7 @@ const Post = props => {
       {/* LikeSection is missing a prop it needs to function! */}
       <LikeSection likePost={() => likePost(post.id)} />
       {/* Comments also wants its props! */}
-      <Comments />
+      <Comments posts={posts}/>
     </div>
   );
 };
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -10,6 +10,11 @@ const Posts = (props) => {
     <div className="posts-container-wrapper">
       {/* map through the posts here to return a Post component */}
       {/* Check the implementation of Post to see what props it requires! */}
+      {
+        posts.map(p => {
+          return <Post key={p.id} likePost={likePost} posts={posts} />
+        })
+      }
     </div>
   );
 };
